13c13ed116d0b7bef2cb798299eb6cdceb451b88
A. S. M. Safiul Sadi <41629377+SafiulSadi@users.noreply.github.com>
2024-06-05 13:25:05 +0600

    Complete day 22

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
 import time
+from scoreboard import Scoreboard
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
@@ -10,6 +11,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 
 screen.listen()
@@ -21,7 +23,7 @@ screen.onkey(r_paddle.go_down, "Down")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
     if ball.ycor() > 280 or ball.ycor() < -280:
@@ -33,10 +35,11 @@ while game_is_on:
     # detect r paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
-
+        scoreboard.l_point()
     # detect l paddle misses
     if ball.xcor() < - 380:
         ball.reset_position()
+        scoreboard.r_point()
 
 
 screen.exitonclick()
